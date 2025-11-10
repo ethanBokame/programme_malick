f61c1ab9365b71ab22ed3ac1f37b21b6ba804a69
ethanBokame <ethaniraqui@gmail.com>
2025-11-10 11:20:23 +0000

    ajout de la fermetture de la modale

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ buttonDiscover.addEventListener('click', () => {
     buttonDiscover.classList.add('hidden');
     program.classList.remove('hidden');
 })
+
+
+window.addEventListener('click', (e) => {
+
+    if (!e.target.closest('.program') && !e.target.closest('.discover-button')) {
+        buttonDiscover.classList.remove('hidden');
+        program.classList.add('hidden');
+    }
+
+    
+})
